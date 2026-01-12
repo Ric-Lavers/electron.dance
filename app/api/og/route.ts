c49375f5e6f110f1cd86093304fc2c0a34b68422
Ric Lavers <ric.lavers@grays.com.au>
2026-01-12 16:52:20 +1100

    add openGraph

--- app/api/og/route.ts
+++ app/api/og/route.ts
@@ -6,6 +6,6 @@ export async function GET() {
   const imageData = await fs.readFile("public/Electron.Dance.png")
   return new ImageResponse(imageData, {
     width: 1200,
-    height: 630,
+    height: 1200,
   })
 }
